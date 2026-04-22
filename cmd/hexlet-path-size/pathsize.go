9cef2f881d3a9cebf2fe567dd38a8a1c9bb26f27
A.Abramovskiy <abramovskiy.anton@gmail.com>
2026-04-22 23:00:38 +0300

    make func for getting path size

---
+++ cmd/hexlet-path-size/pathsize.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+)
+
+func GetPathSize(path string) (int64, error) {
+	var s = int64(0)
+	fi, err := os.Lstat(path)
+	if err != nil {
+		return s, err
+	}
+
+	if fi.IsDir() {
+		entries, err := os.ReadDir(path)
+		if err != nil {
+			return s, err
+		}
+
+		for _, entry := range entries {
+			info, err := entry.Info()
+			if err == nil && !info.IsDir() {
+				s += info.Size()
+			}
+		}
+	} else {
+		s = fi.Size()
+	}
+
+	return s, nil
+}
